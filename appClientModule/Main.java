4903b817556606127cd7865a0156baa6de313898
linxiaoxin1234 <40852290+linxiaoxin1234@users.noreply.github.com>
2019-07-03 15:34:11 +0800

    test wpl

---
+++ appClientModule/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("this is a git test");
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+	//7-3 15:13 测试内容更新 万培林注
+}
