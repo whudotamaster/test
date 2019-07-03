6dd4d4d22ecb3fb85e7446312a5cb70ff4a0d91b
User <User@DESKTOP-GG162FM>
2019-07-03 18:19:10 +0800

    hzr

---
+++ appClientModule/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is test2");
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+		System.out.println("this is test2");
+		System.out.println("this is test2");
+		System.out.println("this is test2");
+		System.out.println("gitTest1 do this test");
+		//////////////////////////////
+		System.out.println("new test");
+		System.out.println("new test2");
+	}
+}
