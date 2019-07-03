5f59d3b2329d34aee23954fb1bfadb041283e8a0
linxiaoxin1234 <40852290+linxiaoxin1234@users.noreply.github.com>
2019-07-03 18:16:51 +0800

    18.16==

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -1,22 +1,22 @@
 
 public class Main {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("this is a git test");
 	}
 
 	/* (non-Java-doc)
 	 * @see java.lang.Object#Object()
 	 */
-	public Main() {
-		super();
-	}
+//	public Main() {
+//		super();
+//	}
 	//7-3 15:13 测试内容更新 万培林注
 	//7-3 18：02测试内容更新 万培林注111111111
-=======
+
 		// TODO Auto-generated method stub
-		System.out.println("this is test2");
-	}
+		
+	
 
 	/* (non-Java-doc)
 	 * @see java.lang.Object#Object()
@@ -30,6 +30,7 @@ public class Main {
 		System.out.println("gitTest1 do this test");
 		//////////////////////////////
 		System.out.println("new test");
+		System.out.println("this is test2");
+		System.out.println("this is test3");
 	}
->>>>>>> branch 'master' of git@github.com:whudotamaster/test.git
 }
