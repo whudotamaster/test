2f986bb1d2152b985940753446d5d5e0a20699bc
z7005881 <z7005881@LAPTOP-IHG1UFUH>
2019-07-03 15:05:48 +0800

    gitTest2

---
+++ appClientModule/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is test2");
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+		System.out.println("this is test2");
+		System.out.println("this is test2");
+		System.out.println("this is test2");
+	}
+
+}
