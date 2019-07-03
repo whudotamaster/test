dbe82a2dbaade173550f2aa5578def200b07b317
z7005881 <z7005881@LAPTOP-IHG1UFUH>
2019-07-03 14:57:19 +0800

    gitTest1

---
+++ appClientModule/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("this is a git test");
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
